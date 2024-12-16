c5a8e5a354950dfe7112a242b434e47edd13224e
Cristhian Joel Iglesias Quezada <Cristhian.13.08.98@gmail.com>
2024-12-15 21:42:16 -0500

    Final

---
+++ src/main/java/pe/edu/cibertec/examan_final_daw1/ExamenFinalDaw1CristhianlglesiasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examan_final_daw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalDaw1CristhianlglesiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalDaw1CristhianlglesiasApplication.class, args);
+	}
+
+}
